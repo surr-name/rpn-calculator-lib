16a0f3f10c720e58d48676ba6d2f212d6cc72cb5
surr <litichevskij.vova@gmail.com>
2021-11-18 15:54:08 +0200

    adds minimal set of essential Matchers

---
+++ src/matchers.ts
@@ -0,0 +1,31 @@
+import {
+    Matcher,
+} from './types';
+
+import {
+    TokenOperand,
+    TokenOperator,
+    TokenUnknown,
+} from './tokens';
+
+export class TokenMatcher implements Matcher {
+    constructor(
+        readonly regexp: Matcher['regexp'],
+        readonly extract: Matcher['extract']
+    ) { }
+}
+
+export const OperandsTokenMatcher = new TokenMatcher(
+    /^[+-]?[0-9]+(?:\.[0-9]+)?(?:\s+|$)/,
+    (matched, location) => new TokenOperand(matched[0], location)
+);
+
+export const FourArithmeticOperatorsTokenMatcher = new TokenMatcher(
+    /^[-+*/](?:\s+|$)/,
+    (matched, location) => new TokenOperator(matched[0], location)
+);
+
+export const UnknownTokenMatcher = new TokenMatcher(
+    /^[-+*/](?:\s+|$)/,
+    (matched, location) => new TokenUnknown(matched[0], location)
+);
